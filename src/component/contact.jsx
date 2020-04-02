983f5672c0b8d2eb1930fd6cdc062e14d83f5dcd
jinggatra <wmkbisa@gmail.com>
2020-04-02 17:08:14 +0700

    done

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import {MDBIcon} from 'mdbreact';
+import fb from '../image/facebook.png';
+import github from '../image/github.png';
+import "../image/image.css";
 
 export default class contact extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="container">
+
+            <div className="white">
+                <p className="font">Contact me on</p>
+                <br></br>
+                {/* <a href="http://www.facebook.com/profile.php">
+                    <img className="photo" src={fb} />
+                </a> */}
+                <a href="https://github.com/jinggatra">
+                    <img className="photo" src={github} />
+                </a>
+
+
             </div>
+        </div>
         )
     }
 }
